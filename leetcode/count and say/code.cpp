407c6589097faaeb54fe907d543b785607e51d1b
chanchal <chanchal14b@gmail.com>
2025-08-08 16:12:16 +0530

    update message

---
+++ leetcode/count and say/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+        string say = countAndSay(n - 1);
+        string result = ""; 
+
+        for(int i = 0; i < say.size(); i++){
+            char ch = say[i];
+            int count = 1;
+            while(i < say.length() - 1 && say[i] == say[i + 1]){
+                count++;
+                i++;
+            }
+            result += to_string(count) + string(1, ch);
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    string output = sol.countAndSay(n);
+    cout << output << endl;
+
+    return 0;
+}
